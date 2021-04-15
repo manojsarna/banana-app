54170e069779abd007450efd4e9113c673b9e433
Manoj Sarna <79917843+manojsarna@users.noreply.github.com>
2021-04-16 01:32:40 +0530

    updated code

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
-var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input")
 
-var outputDiv = document.querySelector("#output");
-
-outputDiv.innerText = "Manoj Sarna"
-
-//console.log(txtInput1)
 
 // DOM SCRIPTING IN INSPECT : STORE AS GLOBAL VARIABLE TO PLAY AROUND WITH ATTRIBUTES
 
-btnTranslate.addEventListener("click",clickEventHandler);
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector('#output');
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
 
-function clickEventHandler(){
-
-    console.log("clicked");
-    console.log("input",txtInput.value)
-    outputDiv.innerText = txtInput.value
-
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
 }
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("There is something with server, come again after sometime")
+}
+
+
+function clickHandler() {
+    //input
+    var inputText = txtInput.value; 
+
+    //processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+           })
+        .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler)
  
