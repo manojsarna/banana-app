a23a74280189e6237e28ec05489a9e0923f523fc
Manoj Sarna <79917843+manojsarna@users.noreply.github.com>
2021-04-15 23:48:00 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input")
 
+var outputDiv = document.querySelector("#output");
+
+outputDiv.innerText = "Manoj Sarna"
+
+//console.log(txtInput1)
+
 // DOM SCRIPTING IN INSPECT : STORE AS GLOBAL VARIABLE TO PLAY AROUND WITH ATTRIBUTES
 
-btnTranslate.addEventListener("click",function clickEventHandler(){
+btnTranslate.addEventListener("click",clickEventHandler);
 
-        console.log("clicked");
-        console.log("input",txtInput.value)
 
-});
+function clickEventHandler(){
+
+    console.log("clicked");
+    console.log("input",txtInput.value)
+    outputDiv.innerText = txtInput.value
+
+}
  
